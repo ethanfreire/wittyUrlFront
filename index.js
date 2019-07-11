c86cd4416e0e26256bb90633acba3c4523e27d6e
villatrue <scarter0313@gmail.com>
2019-07-11 16:16:31 -0400

    streets paid me

--- index.js
+++ index.js
@@ -6,7 +6,94 @@ document.addEventListener("DOMContentLoaded",()=> {
   getNewRowButton().addEventListener("click", (event) => {
     addNewRow(event)
   })
+  getSubmit().addEventListener("click", addToBackend)
 })
+/// add info to backend
+
+function addToBackend(event){
+  addUserToBackend(event)
+  
+}
+
+function fetchUserlinkPost(user_id, baselink_id, url){
+
+  let formData = {
+      user_id: user_id,
+      baselink_id: baselink_id,
+      url: url
+  };
+  
+  let configObject = {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+  };
+  
+  fetch("http://localhost:3000/api/v1/userlinks", configObject)
+      .then(response => response.json())
+      .then(object => {
+          console.log(object)
+      })
+      .catch(error => {
+          window.alert(error.message);
+      });
+
+}
+
+function addUrlToBackend(object){
+  let data = document.querySelectorAll(".formData")
+  console.log(data)
+  let user_id = object.id
+  
+  data.forEach((trElement)=>{
+    debugger
+    let dataSelectValue = trElement.children[0].children[0].value 
+    let dataInputValue = trElement.children[1].children[0].children[0].value
+    fetchUserlinkPost(user_id, dataSelectValue, dataInputValue)
+  })
+  
+  
+  // let baselink_id = document.("socialMediaDropDown").value
+  // let url = document.getElementById("userSocialUrlId").value
+ 
+  
+}
+
+function addUserToBackend(event){
+  // let input = "wow"
+  event.preventDefault()
+  let hello
+  let user = event.target.parentElement.children[0].children[1].value
+  let formData = {
+      username: user
+  };
+  
+  let configObject = {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+  };
+  
+  fetch("http://localhost:3000/api/v1/users", configObject)
+      .then(response => response.json())
+      .then(object => {
+        addUrlToBackend(object) 
+      })
+      .catch(error => {
+          window.alert(error.message);
+      });
+      
+}
+
+
+
+////
 function fetchSocialMediaOptions(){
   fetch("http://localhost:3000/api/v1/baselinks")
   .then(resp => resp.json())
@@ -24,15 +111,14 @@ function addToDropDown(objectSocialMediaOption){
   let rowFix = (rowParsedd - 1)
   let socialMediaOption = document.createElement("option")
   socialMediaOption.id = objectSocialMediaOption.id
-  socialMediaOption.value = objectSocialMediaOption.url
+  socialMediaOption.value = objectSocialMediaOption.id
   socialMediaOption.innerText = objectSocialMediaOption.url
   let dropDownMenu = document.querySelector(`[data-select-id='${rowFix}']`)
   // this line needs to be fixed
   dropDownMenu.append(socialMediaOption)
   
-
 }
-
+////// url  
 function getUserUrl(){
   let userFullUrl = document.querySelector("#userSocialUrlId")
   return userFullUrl
@@ -40,12 +126,15 @@ function getUserUrl(){
 
 function fillOutFullUrl(event){
   console.log(event.target.value)
-  let fullUrl = document.querySelector(".fullUrlSocialSite")
+  let rows = document.querySelector(".socialMediaUrlTable").rows.length
+  let rowParsedd = parseInt(rows)
+  let rowFix = (rowParsedd - 1)
+  let fullUrl = document.querySelector(`[data-full-url-id='${rowFix}']`)
+ 
   let urlSocialMediaSite =document.querySelector("#socialMediaDropDown").value
   fullUrl.innerText = urlSocialMediaSite + "/" +  event.target.value
-
 }
-
+////
 function getNewRowButton(){
   let newRow = document.querySelector("#newRowButton")
   return newRow
@@ -53,29 +142,37 @@ function getNewRowButton(){
 function addNewRow(event){
 
   let newRowContainer = document.createElement("tr")
-  // let currentNumberOfRows = currentNumberOfRows()
-
+  newRowContainer.classList.add("formData")
   let currentNumberOfRows = document.querySelector(".socialMediaUrlTable").rows.length
-  
-// debugger
   newRowContainer.id = currentNumberOfRows
 
   let newRowDropDownMenu = document.createElement("td")
   let newRowSelectMenu = document.createElement("select")
+  //new
   newRowSelectMenu.dataset.selectId = currentNumberOfRows
+  //new
   newRowDropDownMenu.append(newRowSelectMenu)
   newRowContainer.append(newRowDropDownMenu)
 
   let newRowUserSocialSite = document.createElement("td")
+  // newRowUserSocialSite
   let newRowUiForm = document.createElement("div")
   newRowUiForm.classList = "ui form"
   let newRowFormInput = document.createElement("input")
+
+  //////
+  
+  newRowFormInput.addEventListener("change",  fillOutFullUrl)
+  
+
+  /////
   newRowFormInput.placeholder = "Please fill this entry"
   newRowUiForm.append(newRowFormInput)
   newRowUserSocialSite.append(newRowUiForm)
   newRowContainer.append(newRowUserSocialSite)
 
   let newRowFullUrl= document.createElement("td")
+  newRowFullUrl.dataset.fullUrlId = currentNumberOfRows
   let newRowFullUrlDiv = document.createElement("div")
   newRowFullUrlDiv.classList = "field"
   let newRowFullUrlInput = document.createElement("input")
@@ -91,12 +188,13 @@ function addNewRow(event){
 
   fetchSocialMediaOptions()
   
+  
 }
 
 
 //// helpers
 
-// function currentNumberOfRows(){
-//   let rows = document.querySelector(".socialMediaUrlTable").rows.length
-//   return rows
-// }
+function getSubmit(){
+ let getSubmit = document.querySelector("#submitButton")
+ return getSubmit
+}
